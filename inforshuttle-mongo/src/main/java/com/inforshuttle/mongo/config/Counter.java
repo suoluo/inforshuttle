c772fb67423e42b53d505b236e1caef07cc3943c
Administrator <Administrator@DESKTOP-RE8N6K2>
2017-08-26 00:44:18 +0800

    3

--- inforshuttle-mongo/src/main/java/com/inforshuttle/mongo/config/Counter.java
+++ inforshuttle-mongo/src/main/java/com/inforshuttle/mongo/config/Counter.java
@@ -2,46 +2,45 @@ package com.inforshuttle.mongo.config;
 
 import javax.annotation.Resource;
 
-public class Counter {  
-    public  Counter() {  
-    }  
-  
-    public  Counter(double multiplier, String song,Instrument instrument) {  
-        this.multiplier = multiplier;  
-        this.song = song;  
-        this.instrument=instrument;  
-    }  
-  
-    private double multiplier;  
-  
-    private String song;  
-  
-    @Resource  
-    private Instrument instrument;  
-  
-    public double getMultiplier() {  
-        return multiplier;  
-    }  
-  
-    public void setMultiplier(double multiplier) {  
-        this.multiplier = multiplier;  
-    }  
-  
-  
-    public String getSong() {  
-        return song;  
-    }  
-  
-    public void setSong(String song) {  
-        this.song = song;  
-    }  
-  
-    public Instrument getInstrument() {  
-        return instrument;  
-    }  
-  
-    public void setInstrument(Instrument instrument) {  
-        this.instrument = instrument;  
-    }  
-  
-}  
+public class Counter {
+	public Counter() {
+	}
+
+	public Counter(double multiplier, String song, Instrument instrument) {
+		this.multiplier = multiplier;
+		this.song = song;
+		this.instrument = instrument;
+	}
+
+	private double multiplier;
+
+	private String song;
+
+	@Resource
+	private Instrument instrument;
+
+	public double getMultiplier() {
+		return multiplier;
+	}
+
+	public void setMultiplier(double multiplier) {
+		this.multiplier = multiplier;
+	}
+
+	public String getSong() {
+		return song;
+	}
+
+	public void setSong(String song) {
+		this.song = song;
+	}
+
+	public Instrument getInstrument() {
+		return instrument;
+	}
+
+	public void setInstrument(Instrument instrument) {
+		this.instrument = instrument;
+	}
+
+}
